b1fefee03f92effd60c2be23c5bb42ba45c2af6a
Ej Myles <ejelymyles@gmail.com>
2023-05-13 21:06:47 -0700

    tasklister done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector(`form`)
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+   addNewTask(e.target.mytask.value)
+   form.reset()
+  })
+}); 
+
+function addNewTask (task){
+  let p = document.createElement(`p`)
+  p.textContent = `${task} `
+  document.querySelector(`#list`).appendChild(p)
+  let btn = document.createElement(`button`)
+  btn.textContent =  "x"
+  p.appendChild(btn)
+  btn.addEventListener(`click`, deleteTask)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
+
